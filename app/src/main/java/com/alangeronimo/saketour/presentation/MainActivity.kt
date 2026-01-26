9cd6a1c2022f70652e089f5005053d8eb936c778
Jean Pierre Lara <jeanpierre.lara14@gmail.com>
2026-01-25 22:50:21 -0500

    Fix state flows manager and improve event handlers

--- app/src/main/java/com/alangeronimo/saketour/presentation/MainActivity.kt
+++ app/src/main/java/com/alangeronimo/saketour/presentation/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
 import com.alangeronimo.designsystem.ui.theme.SakeTourTheme
 import com.alangeronimo.saketour.presentation.screens.NavHostGenerator
+import com.alangeronimo.saketour.presentation.state.SakeUiEvent
 import com.alangeronimo.saketour.presentation.viewmodel.SakeShopViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,6 +18,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LaunchedEffect(Unit) {
+                viewModel.onEvent(SakeUiEvent.LoadShops)
+            }
+
             SakeTourTheme {
                 Surface {
                     val navController = rememberNavController()
